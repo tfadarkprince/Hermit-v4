147c6e8338bdeba30554346f327fb6b0e975be53
Aadhi <luckyser439@gmail.com>
2025-03-05 05:02:12 +0530

    Add files via upload

---
+++ plugins/channel.js
@@ -0,0 +1,14 @@
+let handler = async m => m.reply(`
+
+╭⭑⭑⭑★✪ *XLICON* ✪★⭑⭑⭑
+│ 📂 *BOT Name:* _XLICON-V2
+│ 📝 *Description:* _I'm XLICON-V2. A MultiDevice WhatsApp bot with rich features Created By Dark prince (Aadhi ser)._
+│ 👤 *Owner:* _Dark prince_
+│ 🌐 *Channel:* https://whatsapp.com/channel/0029VayY2vu1NCrLQHzmRB2Q
+╰━━━━━━━━━━━━━━━━╯
+`.trim())
+handler.help = ['channel']
+handler.tags = ['main']
+handler.command = ['xchannel', 'channel' ] 
+
+export default handler
