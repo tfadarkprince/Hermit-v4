575ed512e2d88b0206537a61f694ec4afab2e6c3
Aadhi <luckyser439@gmail.com>
2025-03-08 23:18:21 +0530

    𝐜𝐫𝐞𝐚𝐭𝐞 𝐛𝐲 𝐝𝐚𝐫𝐤 𝐩𝐫𝐢𝐧𝐜𝐞

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ const DEBUG = convertToBool(process.env.DEBUG, 'true');
 
 module.exports = {
   VERSION: 'v4.4.5',
-  SESSION_ID: process.env.SESSION_ID || 'HP_2025_MAR_8_5M5ERZ',
+  SESSION_ID: process.env.SESSION_ID || '',
   MODE: (process.env.MODE || 'private').toLowerCase(),
   HANDLERS: (process.env.PREFIX || '^[.,!]').trim(),
   SEND_READ: toBool(process.env.READ_COMMAND),
